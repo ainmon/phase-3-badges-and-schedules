b06c5068271de9b9556bf865176231df8060c5e2
Adam <inmon.adam@yahoo.com>
2022-01-20 20:22:23 -0600

    commit tree 409d7843bbec2e1a2853c3277bf048b43c544352

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,23 @@
 # Write your code here.
+require 'pry'
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+    array.map {|n| "Hello, my name is #{n}."}
+end
+
+def assign_rooms(array)
+    array.each_with_index.map {|n, index| "Hello, #{n}! You'll be assigned to room #{index + 1}!"}
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do |d|
+        puts d 
+    end
+    assign_rooms(attendees).each do |a|
+        puts a
+    end
+end
